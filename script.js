c2f84d5af51f2e109b90767affcabf7f4fcea6ff
GaJin Kim <gajin2000@gmail.com>
2022-08-11 01:27:07 -0500

    added more fields to customize render

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
+let iterations = [];
+
+let pixelDensity = Number(document.getElementById("render-quality").value);
+let renderSpeed = Number(document.getElementById("render-speed").value);
 
 function ComplexNumber(real, imaginary) {
     this.real = real;
@@ -18,7 +22,7 @@ ComplexNumber.prototype.absolute = function () {
     return Math.sqrt(this.real * this.real + this.imaginary * this.imaginary);
 }
 
-function belongsToSetInGivenIterations(real, imaginary, iterations) {
+function belongsInSet(real, imaginary, iterations) {
     // z_{n+1} = z_{n}^{2} + c 
     let z = new ComplexNumber(0, 0);
     let c = new ComplexNumber(real, imaginary);
@@ -29,7 +33,7 @@ function belongsToSetInGivenIterations(real, imaginary, iterations) {
         i++;
     }
 
-    return (i == iterations);
+    return i;
 }
 
 function pixelCalculator(x, y, color) {
@@ -37,44 +41,48 @@ function pixelCalculator(x, y, color) {
     context.fillRect(x, y, 1, 1);
 }
 
-function draw(maxIterations) {
+function drawIntervalFrame(maxIterations) {
     // real axis boundaries [-2, 1]
-    let minReal = -2;
-    let maxReal = 1;
-    // imaginary axis boundaries [-1, 1]
-    let minImaginary = -1;
-    let maxImaginary = 1;
+    let minReal = Number(document.getElementById('minreal').value);
+    let maxReal = Number(document.getElementById('maxreal').value);
 
-    let realStep = (maxReal - minReal) / canvas.width;
-    let imaginaryStep = (maxImaginary - minImaginary) / canvas.height;
+    // imaginary axis boundaries [-1, 1]
+    let minImaginary = Number(document.getElementById('minimaginary').value);
+    let maxImaginary = Number(document.getElementById('maximaginary').value);
+
+    let realStep = Number((maxReal - minReal) / canvas.width);
+    let imaginaryStep = Number((maxImaginary - minImaginary) / canvas.height);
 
     let real = minReal;
 
     while (real < maxReal) {
         let imaginary = minImaginary;
+        console.log('boundaries', minReal, maxReal, minImaginary, maxImaginary);
         while (imaginary < maxImaginary) {
-            let result = belongsToSetInGivenIterations(real, imaginary, maxIterations);
+            let pointBelongs = belongsInSet(real, imaginary, maxIterations);
             let x = (real - minReal) / realStep;
             let y = (imaginary - minImaginary) / imaginaryStep;
 
-            // check whether the pixel should be colored black or white
-            result ? pixelCalculator(x, y, 'black') : pixelCalculator(x, y, 'white');
-
+            if (pointBelongs == maxIterations) {
+                pixelCalculator(x, y, 'black');
+            } else {
+                let colorHue = parseInt(30 + Math.round(120 * pointBelongs * 1.0 / maxIterations));
+                var color = `hsl(${colorHue}, 100%, 50%`;
+                pixelCalculator(x, y, color);
+            }
             imaginary += imaginaryStep;
         }
         real += realStep;
     }
 }
 
-// resize canvas to fill browser window dynamically
-window.addEventListener('resize', resizeCanvas, false);
-resizeCanvas();
-
 document.getElementById("render-mandelbrot").onclick = function () {
-    let iterations = [5, 10, 15, 25, 50, 75, 100, 150, 200, 500];
+    updateCanvasSize();
+    updateIterations();
+
     let i = 0;
     let interval = setInterval(function () {
-        draw(iterations[i]);
+        drawIntervalFrame(iterations[i]);
         i++;
         if (i >= iterations.length) {
             clearInterval(interval);
@@ -82,20 +90,51 @@ document.getElementById("render-mandelbrot").onclick = function () {
     }, 1000);
 }
 
-function resizeCanvas() {
+document.getElementById("render-quality").onchange = function () {
+    updateIterations();
+}
+
+// updates canvas size based on available window space
+function updateCanvasSize() {
     // adjust boundaries based on width:height ratio
     let screenRatio = window.innerHeight / window.innerWidth;
     if (screenRatio < 1.5) {
-        console.log('hi');
         canvas.height = window.innerHeight;
         canvas.width = window.innerHeight * (3 / 2);
     } else {
-        console.log('bye');
         canvas.width = window.innerWidth;
         canvas.height = window.innerWidth * (2 / 3);
     }
 
-    // cuts the resolution in third because computer performance sucks
-    canvas.width /= 3;
-    canvas.height /= 3;
+    // cuts the resolution by half because computer performance sucks
+    canvas.width /= 2;
+    canvas.height /= 2;
+}
+
+async function updateIterations() {
+    await updatePixelDensity();
+    await updateRenderSpeed();
+    
+    console.log('pixel density', pixelDensity);
+    console.log('render speed', renderSpeed);
+    
+    iterations.length = 0;
+    while (pixelDensity > 5) {
+        await iterations.unshift(parseInt(pixelDensity));
+        pixelDensity /= renderSpeed;
+    }
+    
+    if (iterations.length == 0) {
+        iterations.push(parseInt(pixelDensity));
+    }
+
+    console.log('iterations', iterations);
+}
+
+function updatePixelDensity() {
+    pixelDensity = Number(document.getElementById("render-quality").value);
+}
+
+function updateRenderSpeed() {
+    renderSpeed = Number(document.getElementById("render-speed").value);
 }
